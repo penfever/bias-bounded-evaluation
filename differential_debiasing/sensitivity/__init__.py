aa84cff22ee048da58ffba6783ca00dde0edd4be
Benjamin Feuer <penfever@gmail.com>
2025-09-05 11:17:09 -0400

    cleanup

--- differential_debiasing/sensitivity/__init__.py
+++ differential_debiasing/sensitivity/__init__.py
@@ -5,25 +5,27 @@ Sensitivity estimation methods for differential debiasing
 from .base import SensitivityEstimator
 from .factor_analysis import FactorAnalysisSensitivity
 from .empirical import EmpiricalSensitivity
-from .cross_validation import CrossValidationSensitivity
-from .historical import HistoricalSensitivity
 from .domain_specific import DomainSpecificSensitivity
 from .psychometric_reliability import PsychometricReliabilitySensitivity
 from .schematic_adherence import SchematicAdherenceSensitivity
 from .combined import CombinedSensitivity
 from .abb_sensitivity import ABBSensitivity
 from .combined_abb_sensitivity import CombinedABBSensitivity
+from .profile_enhanced_abb import ProfileEnhancedABBSensitivity
+from .conformal_sensitivity import ConformalSensitivityEstimator
+from .conformal_bbe_unified import ConformedBiasBoundedPredictor
 
 __all__ = [
     "SensitivityEstimator",
     "FactorAnalysisSensitivity", 
     "EmpiricalSensitivity",
-    "CrossValidationSensitivity",
-    "HistoricalSensitivity", 
     "DomainSpecificSensitivity",
     "PsychometricReliabilitySensitivity",
     "SchematicAdherenceSensitivity", 
     "CombinedSensitivity",
     "ABBSensitivity",
     "CombinedABBSensitivity",
+    "ProfileEnhancedABBSensitivity",
+    "ConformalSensitivityEstimator",
+    "ConformedBiasBoundedPredictor",
 ]
